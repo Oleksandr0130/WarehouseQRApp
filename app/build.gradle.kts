771451d18597e223223b243fc34f6fd395e46104
Oleksandr0130 <olek1995sta@gmail.com>
2025-09-10 20:49:20 +0200

    fix cam to scaner

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,11 +5,11 @@ plugins {
 }
 
 android {
-    namespace = "com.example.flowqr"
+    namespace = "com.flowqr.flowqr"
     compileSdk = 36
 
     defaultConfig {
-        applicationId = "com.example.warehouseqrapp"
+        applicationId = "com.flowqr.flowqr"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
