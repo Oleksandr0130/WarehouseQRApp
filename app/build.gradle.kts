05765a69a3b9d56ae377d9625f8a2e8000c88ece
Oleksandr0130 <olek1995sta@gmail.com>
2025-10-05 12:33:32 +0200

    fix login after restart app

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         applicationId = "com.flowqr.flowqr"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
+        versionCode = 1
         versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
